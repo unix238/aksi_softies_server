1f5ec6f9f7c3fd45c6a76ca963cfc6a8d28a9c6c
unix238 <bekzot.abdyldayev@gmail.com>
2023-02-11 19:48:03 +0600

    cors fixed

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const AuthRouter = require('./routers/authRouter');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/item', ItemRouter);
 app.use('/category', CategoryRouter);
 app.use('/auth', AuthRouter);
