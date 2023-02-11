9ce38f558a88ee9ce56dc0c31a18929efb4c6560
Asylll <asylkhan.orazaly@mail.ru>
2023-02-11 19:22:17 +0600

    edited Item.js

--- models/Item.js
+++ models/Item.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const itemSchema = new mongoose.Schema({
     title: { type: [String], required: true },
-    price: { type: Number, required: true },
+    price: { type: [Number], required: true },
     sizes: { type: [String], required: true },
     description: { type: [String], required: true },
     images: { type: [String], required: true },
