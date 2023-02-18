3556ec90f8e57756f06514b5ea0dd3ab38d7725a
Beka <bekzot.abdyldayev@gmailcom>
2023-02-18 17:12:16 +0600

    fixed imports

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+/** @format */
+
 const config = require('./config/config.js');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const ItemRouter = require('./routers/itemRouter');
-const CategoryRouter = require('./routers/categoryRouter');
-const MaterialRouter = require('./routers/MaterialRouter');
+const ItemRouter = require('./routers/itemRouter.js');
+const CategoryRouter = require('./routers/categoryRouter.js');
+const MaterialRouter = require('./routers/MaterialRouter.js');
 const app = express();
 
 app.use(express.json());
@@ -15,16 +17,16 @@ app.use('/category', CategoryRouter);
 app.use('/material', MaterialRouter);
 
 const start = async () => {
-    try {
-        mongoose.set('strictQuery', false);
-        mongoose.connect(config.DATABASE_URL, {});
+  try {
+    mongoose.set('strictQuery', false);
+    mongoose.connect(config.DATABASE_URL, {});
 
-        app.listen(config.PORT, () => {
-            console.log(`Server has been started on port ${config.PORT}`);
-        });
-    } catch (e) {
-        console.log(e);
-    }
+    app.listen(config.PORT, () => {
+      console.log(`Server has been started on port ${config.PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 start();
